b4a4ba96014e0f2016ae9a42fa3d5f1fd880a6ab
NIKSAM20 <nikhilbisht0026@gmail.com>
2023-03-30 20:44:23 +0530

    Program for calculator

--- src/main/java/Mypkg/Calculator.java
+++ src/main/java/Mypkg/Calculator.java
@@ -6,16 +6,13 @@ public class Calculator {
 	public int add(int x, int y) {
 		return (x+y);
 	}
-	
-	public int sub(int x, int y) {
+		public int sub(int x, int y) {
 		return (x-y);
 	}
-	
-	public int mul(int x, int y) {
+		public int mul(int x, int y) {
 		return (x*y);
 	}
-	
-	public int div(int x, int y) {
+		public int div(int x, int y) {
 		return (x/y);
 	}
 	
